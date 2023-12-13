87735a0bd0fb232d6140f15206a61a8bb9ab509a
xl3ehindTim <timbrouwers1@gmail.com>
2023-12-13 15:30:27 +0100

    Added field

--- parsers/team_log_parser.py
+++ parsers/team_log_parser.py
@@ -1,14 +1,32 @@
-from .base_parser import BaseParser
 import pandas as pd
 
-class TeamParser:
-    def __init__(self, team_data: [BaserParser]):
+from .base_parser import BaseParser
+
+
+class TeamLogParser:
+    def __init__(self, team_data: [BaseParser]):
         self.data = self.__parse_data(team_data)
 
-
     def __parse_data(self, data: [BaseParser]):
-        data_frame = pd.DataFrame()
+        data_frames = []
+
         for i, player in enumerate(data):
-            player["player_id"] = i
-            data_frame = pd.concat(data_frame, data, axis=1)
-        return data_frame.sort_values("liveseconds").reset_index(inplace=True)
+            player_data = (
+                player.data
+            )  # Assuming there is a method to get data from BaseParser
+            player_data["player_id"] = i
+            data_frames.append(player_data)
+
+        result_frame = (
+            pd.concat(data_frames).sort_values("liveseconds").reset_index(drop=True)
+        )
+        return result_frame
+
+    # def __parse_data(self, data: [BaseParser]):
+    #     data_frame = pd.DataFrame()
+
+    #     for i, player in enumerate(data):
+    #         player["player_id"] = i
+    #         data_frame = pd.concat(data_frame, data, axis=1)
+
+    #     return data_frame.sort_values("liveseconds").reset_index(inplace=True)
