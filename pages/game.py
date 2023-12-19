fb7c268a8b4a2b323c7dd87656d9d51916767a26
Tim Brouwers <timbrouwers1@gmail.com>
2023-12-19 10:27:57 +0100

    Added time slider (#4)
    Time bound slider looks good. Approving request

--- pages/game.py
+++ pages/game.py
@@ -10,21 +10,27 @@ from components.charts.game_field import game_field
 register_page(__name__, name="Game", top_nav=True, path="/game")
 
 df = pd.read_csv("data.csv")
+df["liveseconds"] = pd.to_timedelta(df["liveseconds"]).dt.total_seconds()
+
 game = pd.read_csv("game.csv")
 
 
-@callback(Output("ball-graph", "figure"), Input("slider", "value"))
-def ball_location(index):
-    last_position = df.iloc[index]
-
+@callback(Output("ball-graph", "figure"), Input("time-slider", "value"))
+def ball_location(liveseconds):
     # Create a scatter plot
     figure = go.Figure()
+
+    # Find the index of the row with the closest 'liveseconds' to the target_seconds
+    closest_row_index = (df["liveseconds"] - liveseconds).abs().idxmin()
+
+    # Get the closest row
+    closest_row = df.loc[closest_row_index]
 
     # Add trace
     figure.add_trace(
         go.Scatter(
-            x=[last_position["wm_ball_pos_x"]],
-            y=[last_position["wm_ball_pos_y"]],
+            x=[closest_row["wm_ball_pos_x"]],
+            y=[closest_row["wm_ball_pos_y"]],
             mode="markers",
             marker=dict(color="red", size=10),
             name="Last Robot Position",
@@ -48,8 +54,8 @@ def ball_location(index):
     )
 
     # Set layout with axis range based on 'wm_ball_pos_x' and 'wm_ball_pos_y'
-    x_max = max(df["wm_ball_pos_x"].abs().max(), last_position["wm_ball_pos_x"])
-    y_max = max(df["wm_ball_pos_y"].abs().max(), last_position["wm_ball_pos_y"])
+    x_max = max(df["wm_ball_pos_x"].abs().max(), closest_row["wm_ball_pos_x"])
+    y_max = max(df["wm_ball_pos_y"].abs().max(), closest_row["wm_ball_pos_y"])
 
     # Set layout with axis range
     figure.update_layout(
@@ -63,9 +69,27 @@ def ball_location(index):
 
 def layout():
     field = game_field(game)
+
+    num_marks = 30
+
+    # Calculate the step size based on the number of marks
+    step_size = df["liveseconds"].max() / (num_marks - 1)
+
     layout = html.Div(
         [
             html.H1("Game overview"),
+            html.Label("Scale time"),
+            dcc.Slider(
+                id="time-slider",
+                min=0,
+                max=df["liveseconds"].max(),
+                value=0,
+                # marks={i: f"{i//60:02d}:{i%60:02d}" for i in range(0, int(df["liveseconds"].max()) + 1, int(step_size))}
+                marks={
+                    i: f"{int(i//3600):02d}:{int((i%3600)//60):02d}"
+                    for i in range(0, int(df["liveseconds"].max()) + 1, int(step_size))
+                },
+            ),
             dmc.Grid(
                 children=[
                     dmc.Col(
@@ -73,7 +97,7 @@ def layout():
                             dmc.Paper(
                                 shadow="sm",
                                 children=[
-                                    dcc.Graph(id="field", figure=field),
+                                    dcc.Graph(id="field-graph", figure=field),
                                 ],
                             )
                         ],
@@ -85,13 +109,6 @@ def layout():
                                 shadow="sm",
                                 children=[
                                     dcc.Graph(id="ball-graph"),
-                                    html.Label("Scale time"),
-                                    dmc.Slider(
-                                        id="slider",
-                                        min=0,
-                                        max=len(df) - 1,
-                                        value=0,
-                                    ),
                                 ],
                             )
                         ],
