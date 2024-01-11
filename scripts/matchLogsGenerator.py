f482da84d1b2147e4c784a01cdab23db880503bf
A. B. Pancu <58257244+minetrana@users.noreply.github.com>
2024-01-11 10:50:27 +0100

    scrips recommited due to error

--- scripts/matchLogsGenerator.py
+++ scripts/matchLogsGenerator.py
@@ -20,12 +20,14 @@ class Position:
     y: float
     z: float
 
-
+# to be used later for more accurate movement logs
 def calculate_position_with_step(log_time, vel_now, vel_old, pos):
     seconds = int(log_time.hours / (60 * 60) + log_time.minutes / (60) + log_time.seconds) #amount of second since movement started
     new_x = float(pos.x + vel_old.x * seconds + (vel_now.x - vel_old.x) * seconds * seconds / 4)  #new x position
     new_y = float(pos.y + vel_old.y * seconds + (vel_now.y - vel_old.y) * seconds * seconds / 4)  #new y position
     pos_now = Position(new_x, new_y, pos.z)
     return pos_now
+
+
 
 
